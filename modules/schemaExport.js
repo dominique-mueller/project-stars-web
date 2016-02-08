fc44ec2641148def02581bc80d5767515e842686
Marco Roenfeldt <ronfeldtm@gmail.com>
2016-02-08 14:59:30 +0100

    forgot to add all test files

---
+++ modules/schemaExport.js
@@ -0,0 +1,18 @@
+/*
+LOAD ALL SCHEMAS & MODELS
+This makes it possible for other modules to use the ODM models
+*/
+module.exports = {
+	 "Device": require('./device/device.schema.js'),
+	 "Setting": require('./setting/setting.schema.js'),
+	 "Label": require('./label/label.schema.js'),
+	 "Bookmark": require('./bookmark/bookmark.schema.js'),
+	 "User": require('./user/user.schema.js')
+};
+
+
+/*
+How to use in other files:
+	var mongoose = require( 'mongoose' ),  
+    var DBModules = require( '/modules/connectDB.js' ); 
+*/
