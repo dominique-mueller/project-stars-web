691afe0cbb04e2695c056512b9251eff1466a3ec
Dominique Müller <dominique.m.mueller@gmail.com>
2016-02-17 15:02:30 +0100

    Added gulpfile base
    - Added gulpfile with two main tasks (for now)
    - Added babel configuration
    - Updated npm package file with new dev dependencies

---
+++ gulpfile.babel.js
@@ -0,0 +1,31 @@
+/**
+ * Imports
+ */
+import gulp 		from 'gulp';
+import runSequence	from 'run-sequence';
+
+/**
+ * Build development task
+ * ----------------------
+ * TODO: Description here
+ */
+gulp.task( 'build-dev', ( done ) => {
+
+	runSequence(
+		done
+	);
+
+} );
+
+/**
+ * Build production task
+ * ---------------------
+ * TODO: Description here
+ */
+gulp.task( 'build-prod', ( done ) => {
+
+	runSequence(
+		done
+	);
+
+} );
