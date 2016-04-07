8a6ad2d208b3782e3789cc1d806847a2b221124c
Dominique Müller <dominique.m.mueller@gmail.com>
2016-04-07 13:52:41 +0200

    Added label assign dropdown (NOT DONE YET)
    - Added label assign component
    - Added label assign styles
    - Added save functions to bookmark details component

--- public/app/components/bookmark_details/bookmark_details.component.ts
+++ public/app/components/bookmark_details/bookmark_details.component.ts
@@ -1,17 +1,16 @@
 /**
  * External imports
  */
-import { Component, OnInit, Input, Output, EventEmitter } from 'angular2/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange } from 'angular2/core';
 import { List, Map } from 'immutable';
 
 /**
  * Internal imports
  */
 import { BookmarkService, IBookmark } from './../../services/bookmark/bookmark.service';
-import { IFolder } from './../../services/folder/folder.service';
-import { ILabel } from './../../services/label/label.service';
 import { IconComponent } from './../../shared/icon/icon.component';
 import { EditableInputComponent } from './../../shared/editable_input/editable_input.component';
+import { AssignDropdown } from './../../shared/assign_dropdown/assign_dropdown.component';
 
 /**
  * Bookmark details component
@@ -19,7 +18,8 @@ import { EditableInputComponent } from './../../shared/editable_input/editable_i
 @Component( {
 	directives: [
 		IconComponent,
-		EditableInputComponent
+		EditableInputComponent,
+		AssignDropdown
 	],
 	selector: 'app-bookmark-details',
 	templateUrl: './bookmark_details.component.html'
@@ -58,6 +58,8 @@ export class BookmarkDetailsComponent implements OnInit {
 
 	private bookmarkService: BookmarkService;
 
+	private unassignedLabels: any;
+
 	constructor( bookmarkService: BookmarkService ) {
 
 		this.bookmarkService = bookmarkService;
@@ -71,19 +73,60 @@ export class BookmarkDetailsComponent implements OnInit {
 		// TOOD
 	}
 
+	/**
+	 * Call this when input value change
+	 */
+	public ngOnChanges( changes: {
+		[ key: string ]: SimpleChange,
+		labels: SimpleChange
+	} ): void {
+
+		// Skip if we have no data yet
+		if ( typeof this.labels !== 'undefined' && typeof this.data !== 'undefined' ) {
+
+			// Filter: Get all unassigned labels
+			this.unassignedLabels = this.labels.filter( ( label: Map<string, any> ) => {
+				return typeof this.data.get( 'labels' ).get( label.get( 'id' ) ) === 'undefined';
+			} );
+
+		}
+
+	}
+
 	private closePanel(): void {
 		this.close.emit( null );
 	}
 
-	private updateElement( element: Map<string, any>, key: string, value: string ) {
+	/**
+	 * Update element
+	 * @param {Map<string, any>} element Information about the element
+	 * @param {string}           key     Key
+	 * @param {string}           value   Updated value
+	 */
+	private updateElement( element: Map<string, any>, key: string, value: string ): void {
 
-		console.log('### ELEMENT: ' + element);
-		console.log('### KEY: ' + key);
-		console.log('### VALUE: ' + value);
-
+		// Create data
 		let data: any = {};
-		data[key] = value;
+		data[ key ] = value;
 
+		// Update bookmark
+		this.bookmarkService.updateBookmark( element.get( 'id' ), data );
+
+	}
+
+	/**
+	 * Assign new label to element
+	 * @param {Map<string, any>} element  Information about the element
+	 * @param {List<number>}     labels   List of currently assigned label ids
+	 * @param {number}           newLabel New label id (to be assigned)
+	 */
+	private assignLabelToElement( element: Map<string, any>, labels: List<number>, newLabel: number ): void {
+
+		// Create data
+		let data: any = {};
+		data.labels = labels.push( newLabel );
+
+		// Update bookmark
 		this.bookmarkService.updateBookmark( element.get( 'id' ), data );
 
 	}
